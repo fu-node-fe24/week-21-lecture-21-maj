d19d3bc9cce3452db6f44612f01a6ded9fe8b452
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-05-20 23:12:20 +0200

    removed stuff

--- blog-api/server.js
+++ blog-api/server.js
@@ -5,8 +5,7 @@ import mongoose from 'mongoose';
 import keysRouter from './routes/keys.js';
 import authRouter from './routes/auth.js';
 import postsRouter from './routes/posts.js';
-import logger from './middlewares/logger.js';
-import errorHandler from './middlewares/errorHandler.js';
+
 
 // Config
 dotenv.config();
@@ -17,8 +16,6 @@ const database = mongoose.connection;
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
-app.use(logger);
 
 // Routes
 app.use('/api/keys', keysRouter);
@@ -34,5 +31,3 @@ database.once('connected', () => {
         console.log(`Server is running on port ${PORT}`);
     });
 });
-
-app.use(errorHandler);
