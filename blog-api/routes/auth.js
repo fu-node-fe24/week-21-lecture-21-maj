03ae63832b551fa9853941018167f28232ec4c46
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-05-20 22:38:31 +0200

    added start code

---
+++ blog-api/routes/auth.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
+
+const router = Router();
+
+// GET logout
+router.get('/logout', (req, res) => {
+
+});
+
+// POST login
+router.post('/login', async (req, res) => {
+
+});
+
+// POST register
+router.post('/register', async (req, res) => {
+
+});
+
+export default router;
