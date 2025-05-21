f2c8dd775f679a5e1415faaef4940fd7a2f80564
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-05-21 09:49:02 +0200

    kod vid rast

--- blog-api/routes/keys.js
+++ blog-api/routes/keys.js
@@ -1,10 +1,22 @@
 import { Router } from 'express';
+import { getRandomKey } from '../services/keys.js';
 
 const router = Router();
 
 // GET key
 router.get('/', async (req, res) => {
-
+    const key = await getRandomKey();
+    if(key) {
+        res.json({
+            success : true,
+            key : key.key
+        });
+    } else {
+        res.status(404).json({
+            success : false,
+            message : 'No key found'
+        });
+    }
 });
 
 export default router;
---
+++ blog-api/services/keys.js
@@ -0,0 +1,24 @@
+import Key from '../models/key.js';
+
+export async function getRandomKey() {
+    try {
+        const keys = await Key.find();
+        if(keys.length < 1) {
+            throw new Error('No keys found in database');
+        } else {
+            return keys[Math.floor(Math.random() * keys.length)];
+        }
+    } catch(error) {
+        console.log(error.message);
+        return null;
+    }
+}
+
+export async function isKeyInDatabase(key) {
+    try {
+
+    } catch(error) {
+        console.log(error.message);
+        return false;
+    }
+}
